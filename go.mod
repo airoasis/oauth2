a0fb98f7cc4c65a630dae8a9597d5b70d0b3df72
Thomas Kim <jeong.kim@sk.com>
2021-08-03 16:18:16 +0900

    Testing OAuth2

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/airoasis/oauth2
+
+go 1.16
+
+require (
+	github.com/go-oauth2/oauth2/v4 v4.4.1
+	github.com/go-resty/resty/v2 v2.6.0
+	github.com/jackc/pgx/v4 v4.13.0
+	github.com/tidwall/gjson v1.6.0
+	github.com/vgarvardt/go-oauth2-pg/v4 v4.1.0
+	github.com/vgarvardt/go-pg-adapter v1.0.0
+)
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"github.com/go-oauth2/oauth2/v4"
+	"github.com/go-oauth2/oauth2/v4/errors"
+	"github.com/go-oauth2/oauth2/v4/manage"
+	"github.com/go-oauth2/oauth2/v4/models"
+	"github.com/go-oauth2/oauth2/v4/server"
+	"github.com/go-resty/resty/v2"
+	"github.com/jackc/pgx/v4"
+	"github.com/tidwall/gjson"
+	pg "github.com/vgarvardt/go-oauth2-pg/v4"
+	"github.com/vgarvardt/go-pg-adapter/pgx4adapter"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	pgxConn, _ := pgx.Connect(context.TODO(), "postgres://postgres:postgres@localhost:5432/postgres")
+
+	manager := manage.NewDefaultManager()
+
+	adapter := pgx4adapter.NewConn(pgxConn)
+	tokenStore, _ := pg.NewTokenStore(adapter, pg.WithTokenStoreGCInterval(time.Minute))
+	defer tokenStore.Close()
+
+	// client pg store
+	clientStore, _ := pg.NewClientStore(adapter)
+
+	clientStore.Create(&models.Client{
+		ID:     "222222",
+		Secret: "22222222",
+		Domain: "http://localhost:9096",
+	})
+
+	manager.MapTokenStorage(tokenStore)
+	manager.MapClientStorage(clientStore)
+
+	srv := server.NewServer(server.NewConfig(), manager)
+	srv.SetAllowedGrantType(oauth2.PasswordCredentials)
+	srv.SetAllowGetAccessRequest(true)
+
+	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
+		log.Println("Internal Error:", err.Error())
+		return
+	})
+
+	srv.SetResponseErrorHandler(func(re *errors.Response) {
+		log.Println("Response Error:", re.Error.Error())
+	})
+
+	srv.SetPasswordAuthorizationHandler(func(username, password string) (userID string, err error) {
+		client := resty.New()
+
+		resp, err := client.R().
+			SetBody(map[string]interface{}{
+				"username": username,
+				"password": password,
+			}).
+			Post("http://localhost:8080/users/oauth")
+
+		if err != nil {
+			log.Println("ERROR sending the request")
+			return
+		}
+		if resp.StatusCode() == 200 {
+			userID = gjson.Get(resp.String(), "username").String()
+		}
+		return
+	})
+
+	http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
+		srv.HandleTokenRequest(w, r)
+	})
+
+	log.Fatal(http.ListenAndServe(":9096", nil))
+}
